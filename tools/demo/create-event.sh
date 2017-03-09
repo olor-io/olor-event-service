7021856f345bff93ff8b6a67dea45f62180dcc5d
Valentin Abramenkov <valentin.abramenkov@kesko.fi>
2017-03-09 05:08:46 +0200

    Initialized demo data for all HTTP operations

---
+++ tools/demo/create-event.sh
@@ -0,0 +1,1 @@
+curl -X POST -d @tools/json/event.json -H "x-author-id: 23991888" -H "x-token: service" -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:9000/api/v1/events

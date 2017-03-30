3daf953f1fdeef6dbd779db96bff3c2996f3405d
Valentin Abramenkov <valentin.abramenkov@kesko.fi>
2017-03-31 00:01:49 +0300

    Removed unused model

--- src/services/event-service.js
+++ src/services/event-service.js
@@ -3,7 +3,6 @@
 var CONST = require('../constants');
 var Event_ = require('../models/event-model');
 var UserEvent = require('../models/user-event-model');
-var EventDistance = require('../models/event-distance-model');
 var modelUtils = require('../models/model-utils');
 var serviceUtils = require('./service-utils');
 
